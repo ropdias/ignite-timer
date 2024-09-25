318943942f35ab61a5f06825369f3c01fc014f1c
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-09-24 23:43:10 -0300

    using reset from form outside context

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -66,7 +66,6 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-    // reset()
   }
 
   function interruptCurrentCycle() {
