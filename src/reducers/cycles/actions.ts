b5c87361ad0ddc7f4c6e3e399c11c0c865706697
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-10-02 23:19:12 -0300

    adding type CyclesAction

--- src/reducers/cycles/actions.ts
+++ src/reducers/cycles/actions.ts
@@ -6,9 +6,14 @@ export enum ActionTypes {
   MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
 }
 
+export type CyclesAction =
+  | ReturnType<typeof addNewCycleAction>
+  | ReturnType<typeof markCurrentCycleAsFinishedAction>
+  | ReturnType<typeof interruptCurrentCycleAction>
+
 export function addNewCycleAction(newCycle: Cycle) {
   return {
-    type: ActionTypes.ADD_NEW_CYCLE,
+    type: ActionTypes.ADD_NEW_CYCLE as const,
     payload: {
       newCycle,
     },
@@ -17,12 +22,12 @@ export function addNewCycleAction(newCycle: Cycle) {
 
 export function markCurrentCycleAsFinishedAction() {
   return {
-    type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
+    type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED as const,
   }
 }
 
 export function interruptCurrentCycleAction() {
   return {
-    type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
+    type: ActionTypes.INTERRUPT_CURRENT_CYCLE as const,
   }
 }
